6b3ef28833d760c57e50e9d9cbf81c76e55eab57
LINDEX171 <lindex171@gmail.com>
2026-07-30 17:53:02 +0200

    feat: navigation entre les écrans

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -19,15 +19,15 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          title: 'Humeur',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="face.smiling" color={color} />,
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="historique"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: 'Historique',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="clock.arrow.circlepath" color={color} />,
         }}
       />
     </Tabs>
